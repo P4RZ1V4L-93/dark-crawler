3f65f5dfc83aec4ac8336e164087b5530b689e99
P4RZ1V4L-93 <rohit111rahul93@gmail.com>
2022-03-26 22:27:48 +0530

    .

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
+
+profile = FirefoxProfile(r'C:\Users\rohit\Desktop\Tor Browser\Browser\TorBrowser\Data\Browser\profile.default')
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 9050)
+profile.set_preference("network.proxy.socks_remote_dns", False)
+profile.update_preferences()
 
 options = Options()
 options.binary_location = r'C:\Users\rohit\Desktop\Tor Browser\Browser\firefox.exe'
