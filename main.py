2a8bee16af694b6af744d369add7186bdaca0435
P4RZ1V4L-93 <rohit111rahul93@gmail.com>
2022-03-26 22:24:14 +0530

    initital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.binary_location = r'C:\Users\rohit\Desktop\Tor Browser\Browser\firefox.exe'
+
+driver = webdriver.Firefox(firefox_profile=profile, executable_path= r'C:\Users\rohit\Desktop\Python_Projects\Crawler\geckodriver.exe', options=options)
+
+driver.get("http://torchdeedp3i2jigzjdmfpn5ttjhthh5wbmda2rr3jvqjg5p77c54dqd.onion/")
